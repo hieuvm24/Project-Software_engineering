a70e420170398ef1ce4b5a8228c90a3421a4508a
hieuvm24 <traihovunp@gmail.com>
2024-10-03 10:53:40 +0700

    update

--- config.php
+++ config.php
@@ -4,4 +4,4 @@ define("db_user", "root");
 define("db_password", "");
 define("db_name", "tracnghiemonline");
 define("app_path", "/ktpm/");
-define("login_path", "http://localhost/ktpm/auth/signin");
+define("login_path", "http://localhost/Software_engineeringING/auth/signin");
