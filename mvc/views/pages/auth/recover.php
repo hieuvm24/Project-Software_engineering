00ff5475cd9cb0fd7fff3ae61b52a7c915d35944
hieuvm24 <traihovunp@gmail.com>
2024-10-05 16:27:34 +0700

    update

--- mvc/views/pages/auth/recover.php
+++ mvc/views/pages/auth/recover.php
@@ -1,22 +1,14 @@
-<!-- Page Content -->
-<div class="bg-image" style="background-image: url('./public/media/photos/photo16@2x.jpg');">
+<div class="bg-image" style="background-image: url('./public/media/photos/pnk.jpg');">
     <div class="row g-0 justify-content-center bg-black-75">
         <div class="hero-static col-sm-8 col-md-6 col-xl-4 d-flex align-items-center p-2 px-sm-0">
-            <!-- Reminder Block -->
             <div class="block block-transparent block-rounded w-100 mb-0 overflow-hidden">
                 <div class="block-content block-content-full px-lg-5 px-xl-6 py-4 py-md-5 py-lg-6 bg-body-extra-light">
-                    <!-- Header -->
                     <div class="mb-2 text-center">
                         <a class="link-fx fw-bold fs-1" href="index.html">
                             <span class="text-dark">PNK </span><span class="text-primary">Test</span>
                         </a>
                         <p class="text-uppercase fw-bold fs-sm text-muted">Quên mật khẩu</p>
                     </div>
-                    <!-- END Header -->
-
-                    <!-- Reminder Form -->
-                    <!-- jQuery Validation (.js-validation-reminder class is initialized in js/pages/op_auth_reminder.min.js which was auto compiled from _js/pages/op_auth_reminder.js) -->
-                    <!-- For more info and examples you can check out https://github.com/jzaefferer/jquery-validation -->
                     <form class="js-validation-reminder">
                         <div class="mb-4">
                             <div class="input-group input-group-lg">
@@ -35,11 +27,8 @@
                         </div>
                     </form>
                     <input type="hidden" value="" id="email_recover">
-                    <!-- END Reminder Form -->
                 </div>
             </div>
-            <!-- END Reminder Block -->
         </div>
     </div>
 </div>
-<!-- END Page Content -->
